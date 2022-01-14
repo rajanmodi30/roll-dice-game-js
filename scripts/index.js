4363c6a636ae8d9e4d67c38a06586d038a136030
rajan <rajanmodi30@gmail.com>
2022-01-14 22:20:36 +0530

    -shifted from regular functions to es6 functions

--- scripts/index.js
+++ scripts/index.js
@@ -15,19 +15,20 @@ let secondPlayerLateScoreCardElement = document.getElementById(
   "player-2-latest-dice-score"
 );
 
-rollDiceButton.addEventListener("click", rollDice);
-resetButton.addEventListener("click", resetGame);
-
 /**
  *  starts the game and decides between two players on whom to give first chance
  */
-function startGame() {
+const startGame = () => {
   currentPlayer = getRandomArbitrary(1, 3);
   headerElement.textContent = `Player ${currentPlayer} Turn`;
   changeActivePlayerButton();
-}
+};
 
-function rollDice() {
+/**
+ *
+ * @returns roles the dice
+ */
+const rollDice = () => {
   let newDiceValue = getRandomArbitrary(1, 7);
   if (currentPlayer == 1) {
     player1Score += newDiceValue;
@@ -50,20 +51,17 @@ function rollDice() {
   }
   headerElement.textContent = `Player ${currentPlayer} Turn`;
   changeActivePlayerButton();
-}
+};
 
-function decideTurn() {
-  currentPlayer = getRandomArbitrary(1, 2);
-}
-
-function gameOver(whoWon = 1) {
+const gameOver = (whoWon = 1) => {
   rollDiceButton.style.display = "none";
   resetButton.style.display = "block";
   currentPlayer = null;
   headerElement.textContent = `Player ${whoWon} won 🥳`;
   changeActivePlayerButton();
-}
-function resetGame() {
+};
+
+const resetGame = () => {
   rollDiceButton.style.display = "block";
   resetButton.style.display = "none";
   firstPlayerFinalScoreCardElement.textContent = "-";
@@ -73,13 +71,13 @@ function resetGame() {
   player1Score = 0;
   player2Score = 0;
   startGame();
-}
+};
 
-function getRandomArbitrary(min, max) {
+const getRandomArbitrary = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
-function changeActivePlayerButton() {
+const changeActivePlayerButton = () => {
   if (currentPlayer == 1) {
     firstPlayerLateScoreCardElement.parentElement.classList.add("active");
     secondPlayerLateScoreCardElement.parentElement.classList.remove("active");
@@ -90,6 +88,9 @@ function changeActivePlayerButton() {
     firstPlayerLateScoreCardElement.parentElement.classList.remove("active");
     secondPlayerLateScoreCardElement.parentElement.classList.remove("active");
   }
-}
+};
 
 startGame();
+
+rollDiceButton.addEventListener("click", rollDice);
+resetButton.addEventListener("click", resetGame);
